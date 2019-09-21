344f04243f7b0a148c5056c6c6be9db09640f10a
krupalj06 <47038289+krupalj06@users.noreply.github.com>
2019-09-21 13:40:22 +0530

    First Commit
    First Commit for our Xerox Application

---
+++ GitEx/src/com/xworkz/Tester.java
@@ -0,0 +1,14 @@
+package com.xworkz;
+
+public class Tester {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome all .. :)");
+		System.out.println(" Traffic Jam... Please help !!");
+		System.out.println(" Weekend Majja ");
+		System.out.println(" Biometric discussion over");
+		System.out.println(" --- DKS ----");
+		System.out.println( " Rishibavathi ");
+	}
+
+}
