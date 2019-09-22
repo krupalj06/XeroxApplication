e507bfea9c41870574caf8735aa6716951a5560c
krupalj06 <47038289+krupalj06@users.noreply.github.com>
2019-09-22 11:58:19 +0530

    Committed from Git hub directly

--- GitEx/src/com/xworkz/NewAdditionFile.java
+++ GitEx/src/com/xworkz/NewAdditionFile.java
@@ -3,6 +3,8 @@ package com.xworkz;
 public class NewAdditionFile {
 	public static void main(String[] args) {
 		System.out.println(" Doing nothing");
+	
+	System.out.println("This is the change");
 	}
 
 }
